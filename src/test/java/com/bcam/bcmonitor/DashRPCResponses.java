7b555eaacd28a72f5223318fb9533d6db7eb21f0
Max Baylis <24398539+baylism@users.noreply.github.com>
2018-07-16 18:05:30 +0100

    add black dash rpc response file

---
+++ src/test/java/com/bcam/bcmonitor/DashRPCResponses.java
@@ -0,0 +1,24 @@
+package com.bcam.bcmonitor;
+
+public class DashRPCResponses {
+
+
+        public static final String getTransactionResponse = "";
+
+
+        public static final String getBlockResponse = "";
+
+
+        public static final String getBlockchainInfoResponse = "";
+
+
+        public static final String getBestBlockHashResponse = "";
+
+
+        public static final String getBlockHashResppnse = "";
+
+
+        public static final String getMempoolInfoResponse = "";
+
+
+}
