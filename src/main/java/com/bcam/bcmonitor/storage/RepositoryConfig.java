e0977ab6f1ed4d87830e3666b2d4796c698938b6
Max Baylis <24398539+baylism@users.noreply.github.com>
2018-08-21 08:31:38 +0100

    initial attempt

---
+++ src/main/java/com/bcam/bcmonitor/storage/RepositoryConfig.java
@@ -0,0 +1,14 @@
+package com.bcam.bcmonitor.storage;
+
+import com.bcam.bcmonitor.model.BitcoinBlock;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RepositoryConfig {
+
+    // @Bean
+    // public BlockRepository<BitcoinBlock> bitcoinBlockBlockRepository() {
+    //     return  BlockRepository<BitcoinBlock>;
+    // }
+}
