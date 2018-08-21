4a64175d9e5cda906a69e7d44abef24be352aa9f
Max Baylis <24398539+baylism@users.noreply.github.com>
2018-08-21 09:44:38 +0100

    add trans to be interface

--- src/main/java/com/bcam/bcmonitor/extractor/bulk/BulkExtractor.java
+++ src/main/java/com/bcam/bcmonitor/extractor/bulk/BulkExtractor.java
@@ -1,10 +1,13 @@
 package com.bcam.bcmonitor.extractor.bulk;
 
 import com.bcam.bcmonitor.model.BitcoinBlock;
+import com.bcam.bcmonitor.model.BitcoinTransaction;
 import reactor.core.publisher.Flux;
 
 public interface BulkExtractor {
 
     public Flux<BitcoinBlock> saveBlocks(long fromHeight, long toHeight);
+
+    public Flux<BitcoinTransaction> saveTransactions(Flux<BitcoinBlock> blocks);
 
 }
