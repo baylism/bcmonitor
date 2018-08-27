7755c283668e5d4ea462131ea2f19c82a019a1ff
Max Baylis <baylism@tcd.ie>
2018-08-27 20:17:39 +0100

    remove logging in ts/height, add bestblockheight

--- src/main/java/com/bcam/bcmonitor/api/BitcoinController.java
+++ src/main/java/com/bcam/bcmonitor/api/BitcoinController.java
@@ -3,13 +3,13 @@ package com.bcam.bcmonitor.api;
 
 import com.bcam.bcmonitor.extractor.client.ReactiveBitcoinClient;
 import com.bcam.bcmonitor.model.*;
+import com.bcam.bcmonitor.scheduler.BlockchainTracker;
 import com.bcam.bcmonitor.storage.BitcoinBlockRepository;
 import com.bcam.bcmonitor.storage.BlockRepository;
 import com.bcam.bcmonitor.storage.TransactionRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,9 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.concurrent.atomic.AtomicReference;
-
+import static com.bcam.bcmonitor.model.Blockchain.BITCOIN;
 import static org.springframework.data.mongodb.core.aggregation.Aggregation.match;
 import static org.springframework.data.mongodb.core.aggregation.Aggregation.newAggregation;
 
@@ -30,20 +28,19 @@ public class BitcoinController {
     private static final Logger logger = LoggerFactory.getLogger(BitcoinController.class);
 
     private ReactiveBitcoinClient client;
-
-    private ReactiveMongoTemplate template;
     private BlockRepository<BitcoinBlock> blockRepository;
-    // private BitcoinBlockRepository blockRepository;
     private TransactionRepository<BitcoinTransaction> transactionRepository;
+    private BlockchainTracker tracker;
 
 
     @Autowired
-    public BitcoinController(ReactiveBitcoinClient client, BitcoinBlockRepository blockRepository, TransactionRepository<BitcoinTransaction> transactionRepository) {
+    public BitcoinController(ReactiveBitcoinClient client, BitcoinBlockRepository blockRepository, TransactionRepository<BitcoinTransaction> transactionRepository, BlockchainTracker tracker) {
 
         this.client = client;
 
         this.blockRepository = blockRepository;
         this.transactionRepository = transactionRepository;
+        this.tracker = tracker;
     }
 
     // ============ block/transaction requests ============
@@ -59,9 +56,7 @@ public class BitcoinController {
     @GetMapping("/block/{height:[0-9]+}")
     Mono<BitcoinBlock> getBlock(@PathVariable Long height) {
 
-        return blockRepository
-                .findByHeight(height);
-
+        return blockRepository.findByHeight(height);
     }
 
 
@@ -71,81 +66,14 @@ public class BitcoinController {
 
         return blockRepository.
                 findAllByHeightBetweenOrderByHeightAsc(fromHeight - 1, toHeight + 1);
-        // .findAllByHeightInRange(fromHeight, toHeight);
 
     }
 
     @GetMapping(value = "/blocks/{fromHeight}", produces = "application/stream+json")
     Flux<BitcoinBlock> getLatestBlocks(@PathVariable long fromHeight) {
 
-        return blockRepository.findAllByHeightGreaterThan(fromHeight);
+        return blockRepository.findAllByHeightGreaterThan(fromHeight - 1);
     }
-
-
-    // @GetMapping(value = "/blocks/{fromHeight}", produces = "application/stream+json")
-    // Flux<BitcoinBlock> getLatestBlocks(@PathVariable long fromHeight) {
-    //
-    //     // UnicastProcessor<BitcoinBlock> hotSource = UnicastProcessor.create();
-    //     //
-    //     // Flux<BitcoinBlock> hotFlux = hotSource.publish()
-    //     //         .autoConnect();
-    //     //
-    //     //
-    //     //
-    //     // return hotFlux;
-    //
-    //
-    //     AtomicReference<Long> reached = new AtomicReference<>(fromHeight);
-    //
-    //     return Flux.generate(
-    //             () -> fromHeight,
-    //             (state, sink) -> {
-    //
-    //                 blockRepository
-    //                         .findAllByHeightGreaterThan(state)
-    //                         .subscribe(block -> {
-    //
-    //                             if (block.getHeight() > reached.get()){
-    //                                 reached.set(block.getHeight());
-    //                                 sink.next(block);
-    //                             }
-    //                         });
-    //
-    //                 // if (state == 10) sink.complete();
-    //                 return reached.get();
-    //             });
-    //  // AtomicReference<Long> reached = new AtomicReference<>(fromHeight);
-    //  //
-    //  //    return Flux.generate(
-    //  //            () -> fromHeight,
-    //  //            (state, sink) -> {
-    //  //
-    //  //
-    //  //                Flux<BitcoinBlock> newBlocks = blockRepository
-    //  //
-    //  //                        .findAllByHeightGreaterThan(state)
-    //  //                        .doOnNext(block -> {
-    //  //
-    //  //                            if (block.getHeight() > reached.get()){
-    //  //                                reached.set(block.getHeight());
-    //  //                            }
-    //  //
-    //  //                        });
-    //  //
-    //  //                newBlocks.subscribe(sink::next);
-    //  //
-    //  //                // if (state == 10) sink.complete();
-    //  //                return reached.get();
-    //  //            });
-    //
-    //     // return flux;
-    //     // return blockRepository.
-    //     //         findAllByHeightGreaterThanOrderByHeightAsc(fromHeight - 1)
-    //     //         .switchIfEmpty();
-    //
-    //     // .findAllByHeightInRange(fromHeight, toHeight);
-    //
-    // }
 
 
     @GetMapping("/transaction/{hash}")
@@ -162,15 +90,16 @@ public class BitcoinController {
 
         Flux<String> ids = blockRepository
                 .findByHeight(blockHeight)
-                // .doOnNext( b -> logger.info("found block" + b))
                 .map(AbstractBlock::getTxids)
-                // .doOnNext( b -> logger.info("got tx" + b))
                 .flatMapMany(Flux::fromIterable);
-        // .doOnNext( b -> logger.info("tx flux block" + b));
-
-        // return ids;
 
         return transactionRepository.findAllById(ids);
+    }
+
+    @GetMapping("bestblockheight")
+    Mono<Long> getBestHeight() {
+
+        return Mono.justOrEmpty(tracker.getTipFor(BITCOIN));
     }
 
     // ============ other objects ============
