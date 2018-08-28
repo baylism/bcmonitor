13b78d7d51d077fb543bf8a0a54faf95eec66b60
Max Baylis <baylism@tcd.ie>
2018-08-28 11:39:09 +0100

    update auth in getinfo

--- scripts/getinfo.sh
+++ scripts/getinfo.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-#curl --user --user ${adminuser}:${adminpw} http://localhost:8080/admin/extraction | python -m json.tool
-curl -s --user --user ${adminuser}:${adminpw} http://35.229.87.236/admin/extraction | jq -r .
+curl -s --user ${adminuser}:${adminpw} http://35.229.87.236/admin/extraction | jq -r .
